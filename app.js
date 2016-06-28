1d99fd54dbe27c9375bdad060c4aaaafd720a60e
Kool Havis <havisk@gmail.com>
2016-06-28 01:56:51 -0400

    hmm

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());z
 
-app.use(express.static(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
